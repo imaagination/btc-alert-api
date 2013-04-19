12ad0cc26f8bc726583c53903c742c24017be9dc
Peter Maag <maag.peter@gmail.com>
2013-04-18 21:41:05 -0600

    added a management script

---
+++ start.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+require 'thin'
+require File.dirname(__FILE__) + '/app/alert_api'
+
+server = ::Thin::Server.new('127.0.0.1', 9876, App)
+server.log_file = 'thin.log'
+server.pid_file = 'thin.pid'
+server.daemonize
+server.start
